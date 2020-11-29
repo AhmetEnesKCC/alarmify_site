d6440173eeff85e190337939de631ec8b572f823
AhmetEnesKCC <ahmetkcc2001@gmail.com>
2020-11-29 23:16:27 +0300

    fixed link

--- src/components/github_link.js
+++ src/components/github_link.js
@@ -3,9 +3,16 @@ import GithubLogo from "./github_logo";
 export default function GithubLink() {
   return (
     <div className='github_link'>
-      <div className='link_container'>
+      <div
+        className='link_container'
+        onClick={() => document.getElementById("link").click()}
+      >
         <GithubLogo />
-        <a target='_blank' href='https://github.com/AhmetEnesKCC/alarmify_app'>
+        <a
+          target='_blank'
+          id='link'
+          href='https://github.com/AhmetEnesKCC/alarmify_app'
+        >
           alarmify_app
         </a>
       </div>
